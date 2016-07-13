2757fc68ef62e62df021199c11cc0b4c2296494b
mb1a15 <mb4e10@soton.ac.uk>
2016-07-13 14:49:11 +0100

    Add __init__.py

---
+++ micromagneticmodel/energies/__init__.py
@@ -0,0 +1,6 @@
+from .exchange import Exchange
+from .zeeman import Zeeman
+from .demag import Demag
+from .dmi import DMI
+from .anisotropies import UniaxialAnisotropy
+from .hamiltonian import Hamiltonian

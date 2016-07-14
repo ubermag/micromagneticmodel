9c2df65b0a9e4009963c13146b2eb52c7f96d463
mb1a15 <mb4e10@soton.ac.uk>
2016-07-14 15:45:12 +0100

    Updates.

---
+++ micromagneticmodel/energies/uniaxialanisotropyabstract.py
@@ -0,0 +1,25 @@
+import numpy as np
+from numbers import Real
+from energyterm import EnergyTerm
+
+
+class UniaxialAnisotropyAbstract(EnergyTerm):
+    latex_str = '$K (\mathbf{m} \cdot \mathbf{u})^{2}$'
+
+    def __init__(self, K, u):
+        """A uniaxial anisotropy energy abstract class.
+
+        Args:
+            K (Real): Uniaxial anisotropy energy constant (J/m**3)
+            u (tuple, list, np.ndarray): Easy axis
+
+        """
+        if not isinstance(K, Real):
+            raise ValueError('K must be a positive real number.')
+        if not isinstance(u, (tuple, list, np.ndarray)) or len(u) != 3:
+            raise ValueError('u must be a 3-element tuple, '
+                             'list, or np.ndarray.')
+        if not all([isinstance(i, Real) for i in u]):
+            raise ValueError('All elements of u must be real numbers.')
+        self.K = K
+        self.u = u
---
+++ micromagneticmodel/tests/test_uniaxialanisotropyabstract.py
@@ -0,0 +1,57 @@
+import pytest
+import numpy as np
+from numbers import Real
+from micromagneticmodel.energies import UniaxialAnisotropyAbstract
+
+
+class UniaxialAnisotropy(UniaxialAnisotropyAbstract):
+    """Implementation of the abstract class for testing."""
+    def calculator_script(self):
+        raise NotImplementedError()
+
+
+class TestUniaxialAnisotropy(object):
+    def setup(self):
+        self.valid_args = [(1, (0, 1, 0)),
+                           (5e6, [1, 1, 1]),
+                           (-25.6e-3, np.array([0, 0, 1])),
+                           (1.5, [1e6, 1e6, 5e9])]
+        self.invalid_args = [('1', (0, 1, 0)),
+                             (5e6, '(1, 1, 1)'),
+                             (1e-3, (0, 0, 0, 1)),
+                             (5, 5.0)]
+
+    def test_init_valid_args(self):
+        for arg in self.valid_args:
+            K = arg[0]
+            u = arg[1]
+            anisotropy = UniaxialAnisotropy(K, u)
+
+            assert anisotropy.K == K
+            assert isinstance(anisotropy.K, Real)
+            assert isinstance(anisotropy.u, (tuple, list, np.ndarray))
+            assert len(u) == 3
+            assert all([isinstance(i, Real) for i in anisotropy.u])
+
+    def test_init_invalid_args(self):
+        for arg in self.invalid_args:
+            K = arg[0]
+            u = arg[1]
+            with pytest.raises(ValueError):
+                anisotropy = UniaxialAnisotropy(K, u)
+
+    def test_repr_latex(self):
+        for arg in self.valid_args:
+            K = arg[0]
+            u = arg[1]
+            anisotropy = UniaxialAnisotropy(K, u)
+            latex_str = anisotropy._repr_latex_()
+
+            # Assert some characteristics of LaTeX string.
+            assert isinstance(latex_str, str)
+            assert latex_str[0] == latex_str[-1] == '$'
+            assert 'K' in latex_str
+            assert '\mathbf{u}' in latex_str
+            assert '\mathbf{m}' in latex_str
+            assert '^{2}' in latex_str
+            assert '\cdot' in latex_str

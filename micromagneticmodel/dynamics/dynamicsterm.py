e10479875624efe1db3f3e1d22a46862b8af0e6e
mb1a15 <mb4e10@soton.ac.uk>
2016-07-19 13:45:23 +0100

    An attempt to fix imports.

--- micromagneticmodel/dynamics/dynamicsterm.py
+++ micromagneticmodel/dynamics/dynamicsterm.py
@@ -4,8 +4,8 @@ from micromagneticmodel.util import Term
 class DynamicsTerm(Term):
     def __add__(self, other):
         """Addition for creating a list of energy objects."""
-        import dynamics
-        result = dynamics.Dynamics()
+        from micromagneticmodel.dynamics import Dynamics
+        result = Dynamics()
         result.add(self)
         result.add(other)
         return result
--- micromagneticmodel/hamiltonian/energyterm.py
+++ micromagneticmodel/hamiltonian/energyterm.py
@@ -4,8 +4,8 @@ from micromagneticmodel.util import Term
 class EnergyTerm(Term):
     def __add__(self, other):
         """Addition for creating a list of energy objects."""
-        import hamiltonian
-        result = hamiltonian.Hamiltonian()
-        result.add(self)
-        result.add(other)
-        return result
+        from micromagneticmodel.hamiltonian import Hamiltonian
+        hamiltonian = Hamiltonian()
+        hamiltonian.add(self)
+        hamiltonian.add(other)
+        return hamiltonian

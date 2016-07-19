79e5bf7b16aaf5c388a9eebdba8fe84b30a9392f
mb1a15 <mb4e10@soton.ac.uk>
2016-07-19 10:38:09 +0100

    Rewrite imports.

--- micromagneticmodel/dynamics/dynamicsterm.py
+++ micromagneticmodel/dynamics/dynamicsterm.py
@@ -1,11 +1,11 @@
+import dynamics
 from micromagneticmodel.util import Term
-from dynamics import Dynamics
 
 
 class DynamicsTerm(Term):
     def __add__(self, other):
         """Addition for creating a list of energy objects."""
-        dynamics = Dynamics()
-        dynamics.add(self)
-        dynamics.add(other)
-        return dynamics
+        result = dynamics.Dynamics()
+        result.add(self)
+        result.add(other)
+        return result
--- micromagneticmodel/hamiltonian/energyterm.py
+++ micromagneticmodel/hamiltonian/energyterm.py
@@ -1,11 +1,11 @@
+import hamiltonian
 from micromagneticmodel.util import Term
-from hamiltonian import Hamiltonian
 
 
 class EnergyTerm(Term):
     def __add__(self, other):
         """Addition for creating a list of energy objects."""
-        hamiltonian = Hamiltonian()
-        hamiltonian.add(self)
-        hamiltonian.add(other)
-        return hamiltonian
+        result = hamiltonian.Hamiltonian()
+        result.add(self)
+        result.add(other)
+        return result

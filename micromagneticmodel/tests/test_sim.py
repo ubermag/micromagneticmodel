9f4affa5d661a4c5b54342e1a8c6039afffe29a2
Hans Fangohr <fangohr@soton.ac.uk>
2016-08-06 21:24:54 +0100

    Add test of Simulation class
    (minimal)

---
+++ micromagneticmodel/tests/test_sim.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+@pytest.fixture
+def mesh():
+    from micromagneticmodel.mesh import Mesh
+    cmin = (0, 0, 0)
+    cmax = (1., 1., 1.)
+    #d = (2, 2, 2)
+    d = (0.2, 0.2, 0.2)
+
+    mymesh = Mesh(cmin, cmax, d)
+
+    return mymesh
+    
+
+
+def test_sim(mesh):
+    from micromagneticmodel.sim import Sim
+    Ms = 8.6e5
+
+    sim = Sim(mesh, Ms, name='test_sim')
+    assert sim
+

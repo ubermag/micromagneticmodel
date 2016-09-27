bd1b28951e31371b747aa87cd4907b405d083b15
mb1a15 <mb4e10@soton.ac.uk>
2016-09-27 11:54:52 +0100

    Remove two methods from driver.

--- micromagneticmodel/drivers/driver.py
+++ micromagneticmodel/drivers/driver.py
@@ -8,9 +8,3 @@ class Driver:
 
     def script(self):
         raise NotImplementedError
-
-    def run_simulator(self):
-        raise NotImplementedError
-
-    def update_system(self):
-        raise NotImplementedError
--- micromagneticmodel/tests/test_driver.py
+++ micromagneticmodel/tests/test_driver.py
@@ -18,11 +18,3 @@ class TestDriver:
     def test_script(self):
         with pytest.raises(NotImplementedError):
             self.driver.script()
-
-    def test_run_simulator(self):
-        with pytest.raises(NotImplementedError):
-            self.driver.run_simulator()
-
-    def test_update_system(self):
-        with pytest.raises(NotImplementedError):
-            self.driver.update_system()

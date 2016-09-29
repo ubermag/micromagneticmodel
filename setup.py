0cd68ffe20b40075508984b697c3a579a9050615
mb1a15 <mb4e10@soton.ac.uk>
2016-09-29 13:08:24 +0100

    Update setup.py.

--- setup.py
+++ setup.py
@@ -4,22 +4,23 @@ with open('README.rst') as f:
     readme = f.read()
 
 setup(
-    name='micromagneticmodel',
-    version='0.1',
-    description='A Python-based micromagnetic model.',
+    name="micromagneticmodel",
+    version="0.1",
+    description="A Python-based micromagnetic model.",
     long_description=readme,
-    author='Computational Modelling Group',
-    author_email='fangohr@soton.ac.uk',
-    url='https://github.com/joommf/micromagneticmodel',
-    download_url='https://github.com/joommf/micromagneticmodel/tarball/0.1',
-    packages=['micromagneticmodel',
-              'micromagneticmodel.util',
-              'micromagneticmodel.hamiltonian',
-              'micromagneticmodel.dynamics',
-              'micromagneticmodel.drivers',
-              'micromagneticmodel.tests'],
+    author="Computational Modelling Group",
+    author_email="fangohr@soton.ac.uk",
+    url="https://github.com/joommf/micromagneticmodel",
+    download_url="https://github.com/joommf/micromagneticmodel/tarball/0.1",
+    packages=["micromagneticmodel",
+              "micromagneticmodel.util",
+              "micromagneticmodel.hamiltonian",
+              "micromagneticmodel.dynamics",
+              "micromagneticmodel.drivers",
+              "micromagneticmodel.tests"],
+    install_requires=["discretisedfield"],
     classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
     ]
 )

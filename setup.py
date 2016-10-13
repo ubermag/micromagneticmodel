0ce73517a99f28f23a5c4587a12706c0d2c313f3
mb1a15 <mb4e10@soton.ac.uk>
2016-10-13 16:27:07 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.rst') as f:
 
 setup(
     name="micromagneticmodel",
-    version="0.5.2",
+    version="0.5.4",
     description="A Python-based micromagnetic model.",
     long_description=readme,
     author="Computational Modelling Group",
     author_email="fangohr@soton.ac.uk",
     url="https://github.com/joommf/micromagneticmodel",
-    download_url="https://github.com/joommf/micromagneticmodel/tarball/0.5.2",
+    download_url="https://github.com/joommf/micromagneticmodel/tarball/0.5.4",
     packages=["micromagneticmodel",
               "micromagneticmodel.util",
               "micromagneticmodel.hamiltonian",

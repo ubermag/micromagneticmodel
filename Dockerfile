abe7f28cbc3be4b6be63825eb17313f16f7e6563
mb1a15 <mb4e10@soton.ac.uk>
2016-10-18 13:40:34 +0100

    Tidy up Dockerfile after nbval is updated in PyPI.

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM ubuntu:16.04
 
 RUN apt-get update -y
 RUN apt-get install -y git python3-pip curl
-RUN python3 -m pip install --upgrade pip pytest-cov \
-      git+git://github.com/computationalmodelling/nbval.git nbformat \
+RUN python3 -m pip install --upgrade pip pytest-cov nbval \
       git+git://github.com/joommf/joommfutil.git \
       git+git://github.com/joommf/discretisedfield.git
 
 WORKDIR /usr/local/
-
 RUN git clone https://github.com/joommf/micromagneticmodel.git
+
+WORKDIR /usr/local/micromagneticmodel
--- setup.py
+++ setup.py
@@ -1,24 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.rst') as f:
     readme = f.read()
 
 setup(
-    name="micromagneticmodel",
-    version="0.5.4.2",
-    description="A Python-based micromagnetic model.",
+    name='micromagneticmodel',
+    version='0.5.4.3',
+    description='A Python-based micromagnetic model.',
     long_description=readme,
-    author="Computational Modelling Group",
-    author_email="fangohr@soton.ac.uk",
-    url="https://github.com/joommf/micromagneticmodel",
-    download_url="https://github.com/joommf/micromagneticmodel/tarball/0.5.4.2",
-    packages=["micromagneticmodel",
-              "micromagneticmodel.util",
-              "micromagneticmodel.hamiltonian",
-              "micromagneticmodel.dynamics",
-              "micromagneticmodel.drivers",
-              "micromagneticmodel.tests"],
-    install_requires=["discretisedfield"],
-    classifiers=["License :: OSI Approved :: BSD License",
-                 "Programming Language :: Python :: 3"]
+    url='https://github.com/joommf/micromagneticmodel',
+    author='Computational Modelling Group',
+    author_email='fangohr@soton.ac.uk',
+    download_url='https://github.com/joommf/micromagneticmodel/tarball/0.5.4.3',
+    packages=find_packages(),
+    install_requires=['discretisedfield'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: BSD License',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Intended Audience :: Science/Research',
+                 'Programming Language :: Python :: 3 :: Only']
 )

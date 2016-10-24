ec37254841efbd9a9dcac59b8fa43283262c8fea
mb1a15 <mb4e10@soton.ac.uk>
2016-10-24 16:10:16 +0100

    Minor change.

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import setuptools
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
 setuptools.setup(
-    name='micromagneticmodel',
-    version='0.5.4.4',
-    description='A Python-based micromagnetic model.',
+    name="micromagneticmodel",
+    version="0.5.6",
+    description="A Python-based micromagnetic model.",
     long_description=readme,
-    url='https://joommf.github.io',
-    author='Marijan Beg, Ryan A. Pepper, and Hans Fangohr',
-    author_email='jupyteroommf@gmail.com',
+    url="https://joommf.github.io",
+    author="Marijan Beg, Ryan A. Pepper, and Hans Fangohr",
+    author_email="jupyteroommf@gmail.com",
     packages=setuptools.find_packages(),
-    install_requires=['discretisedfield'],
-    classifiers=['Development Status :: 3 - Alpha',
-                 'License :: OSI Approved :: BSD License',
-                 'Topic :: Scientific/Engineering :: Physics',
-                 'Intended Audience :: Science/Research',
-                 'Programming Language :: Python :: 3 :: Only']
+    install_requires=["discretisedfield"],
+    classifiers=["Development Status :: 3 - Alpha",
+                 "License :: OSI Approved :: BSD License",
+                 "Topic :: Scientific/Engineering :: Physics",
+                 "Intended Audience :: Science/Research",
+                 "Programming Language :: Python :: 3 :: Only"]
 )

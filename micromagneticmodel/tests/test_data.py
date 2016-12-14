81a1f005e8ee2f27faa42a89bb6aa33bca2a607c
mb1a15 <m.beg@soton.ac.uk>
2016-12-14 15:46:31 +0000

    Add abstract data tests.

---
+++ micromagneticmodel/tests/test_data.py
@@ -0,0 +1,19 @@
+import pytest
+import micromagneticmodel as mm
+
+
+class TestData:
+    def setup(self):
+        self.data = mm.Data(system=None, interaction=None)
+
+    def test_energy(self):
+        with pytest.raises(NotImplementedError):
+            energy = self.data.energy
+
+    def test_energy_density(self):
+        with pytest.raises(NotImplementedError):
+            energy = self.data.energy_density
+
+    def test_effective_field(self):
+        with pytest.raises(NotImplementedError):
+            energy = self.data.effective_field

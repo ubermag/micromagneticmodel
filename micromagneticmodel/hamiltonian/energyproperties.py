70765fc0bca62f32471ac32b3aa318132c588263
mb1a15 <m.beg@soton.ac.uk>
2016-12-14 15:40:49 +0000

    PEP8

--- micromagneticmodel/hamiltonian/energyproperties.py
+++ micromagneticmodel/hamiltonian/energyproperties.py
@@ -4,12 +4,12 @@ import importlib
 class EnergyProperties:
     @property
     def _data(self):
-        selfmodule = importlib.__import__(self.__class__.__module__)
+        module = importlib.__import__(self.__class__.__module__)
         if self.__class__.__name__ == "Hamiltonian":
-            return selfmodule.Data(self._system, self.__class__.__name__)
+            return module.Data(self._system, self.__class__.__name__)
         else:
-            return selfmodule.Data(self._termsum._system, self.__class__.__name__)
-    
+            return module.Data(self._termsum._system, self.__class__.__name__)
+
     @property
     def energy(self):
         return self._data.energy

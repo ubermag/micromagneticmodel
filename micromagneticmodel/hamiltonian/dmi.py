1fe9cc6223982ebd746e4dac9b7d1e03e162bf91
mb1a15 <m.beg@soton.ac.uk>
2017-02-01 12:04:07 +0000

    Add abstract DMI class.

---
+++ micromagneticmodel/hamiltonian/dmi.py
@@ -0,0 +1,28 @@
+import joommfutil.typesystem as ts
+from .energyterm import EnergyTerm
+
+
+@ts.typesystem(D=ts.Real,
+               name=ts.ObjectName)
+class DMI(EnergyTerm):
+    _latex = ("$D \mathbf{m}(\\nabla \\times \mathbf{m})$")
+
+    def __init__(self, D, name="dmi"):
+        """A DMI energy class.
+
+        Args:
+            D (Real): DMI energy constant (J/m**2)
+
+        """
+        self.D = D
+        self.name = name
+
+    @property
+    def _repr(self):
+        """A representation string property.
+
+        Returns:
+           A representation string.
+
+        """
+        return "DMI(D={})".format(self.D)

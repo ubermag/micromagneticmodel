4c031cac36dc8dfe107f568f813705c943139958
Marijan Beg <m.beg@soton.ac.uk>
2018-06-29 22:54:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
-with open("README.rst") as f:
+with open("README.md", 'r+', encoding="utf-8") as f:
     readme = f.read()
 
 setuptools.setup(
     name="micromagneticmodel",
     version="0.8",
-    description="A Python-based micromagnetic model.",
+    description="Python utilities package used across all JOOMMF packages.",
     long_description=readme,
     url="https://joommf.github.io",
-    author="Marijan Beg and Hans Fangohr",
+    author="Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr",
     author_email="jupyteroommf@gmail.com",
     packages=setuptools.find_packages(),
     install_requires=["discretisedfield"],

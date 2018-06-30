1916721af5593b362521fcf574c675689ebb83c9
Marijan Beg <m.beg@soton.ac.uk>
2018-06-30 19:41:08 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="micromagneticmodel",
-    version="0.8",
-    description=("Python based micromagnetic model providing interface "
-                 "to micromagnetic simulation tools."),
+    name='micromagneticmodel',
+    version='0.8.1',
+    description=('Python based micromagnetic model providing interface '
+                 'to micromagnetic simulation tools.'),
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://joommf.github.io',
     author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
     author_email='jupyteroommf@gmail.com',
     packages=setuptools.find_packages(),
-    install_requires=["discretisedfield"],
+    install_requires=['discretisedfield'],
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: BSD License',
                  'Programming Language :: Python :: 3 :: Only',

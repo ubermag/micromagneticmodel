ba3925deb5137998747ef388817ddacd921b1ba7
Marijan Beg <m.beg@soton.ac.uk>
2018-06-30 19:36:08 +0200

    Update setup.py for Markdown README.

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 import setuptools
 
-with open("README.md", 'r+', encoding="utf-8") as f:
-    readme = f.read()
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="micromagneticmodel",
     version="0.8",
-    description="Python utilities package used across all JOOMMF packages.",
-    long_description=readme,
-    url="https://joommf.github.io",
-    author="Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr",
-    author_email="jupyteroommf@gmail.com",
+    description=("Python based micromagnetic model providing interface "
+                 "to micromagnetic simulation tools."),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://joommf.github.io',
+    author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
+    author_email='jupyteroommf@gmail.com',
     packages=setuptools.find_packages(),
     install_requires=["discretisedfield"],
-    classifiers=["Development Status :: 3 - Alpha",
-                 "License :: OSI Approved :: BSD License",
-                 "Topic :: Scientific/Engineering :: Physics",
-                 "Intended Audience :: Science/Research",
-                 "Programming Language :: Python :: 3 :: Only"]
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Intended Audience :: Science/Research',
+                 'Natural Language :: English']
 )

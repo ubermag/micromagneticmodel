a4ee02c070aa2e91a582c8887a532fb9d09598a5
Marijan Beg <marijan.beg@xfel.eu>
2019-08-13 00:29:28 +0200

    Increase version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='micromagneticmodel',
-    version='0.8.4',
+    version='0.8.5',
     description=('Python domain-specific language for '
                  'defining micromagnetic models'),
     long_description=long_description,

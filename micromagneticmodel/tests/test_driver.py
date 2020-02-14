2f2206981a7f8b9b0a5e3b02d6538af09325072b
Marijan Beg <m.beg@soton.ac.uk>
2020-02-14 14:39:26 +0000

    PEP8.

--- micromagneticmodel/tests/test_driver.py
+++ micromagneticmodel/tests/test_driver.py
@@ -5,12 +5,12 @@ import micromagneticmodel as mm
 class MyDriver(mm.Driver):
     _allowed_kwargs = ['arg1', 'arg2']
 
-    
+
 def test_init():
     driver = MyDriver(arg1=1, arg2='abc')
     assert driver.arg1 == 1
     assert driver.arg2 == 'abc'
-    
+
     with pytest.raises(AttributeError):
         driver = MyDriver(arg1=1, arg2='abc', arg3=3)
 
--- micromagneticmodel/tests/test_evolver.py
+++ micromagneticmodel/tests/test_evolver.py
@@ -5,12 +5,12 @@ import micromagneticmodel as mm
 class MyEvolver(mm.Evolver):
     _allowed_kwargs = ['arg1', 'arg2']
 
-    
+
 def test_init():
     evolver = MyEvolver(arg1=1, arg2='abc')
     assert evolver.arg1 == 1
     assert evolver.arg2 == 'abc'
-    
+
     with pytest.raises(AttributeError):
         evolver = MyEvolver(arg1=1, arg2='abc', arg3=3)
 

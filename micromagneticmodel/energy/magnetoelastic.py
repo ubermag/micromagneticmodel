2a50ee22c205d1fd7f695fb746088d7013e9fb7f
Marijan Beg <m.beg@soton.ac.uk>
2020-03-07 18:42:25 +0000

    Split strain matrix.

--- micromagneticmodel/energy/magnetoelastic.py
+++ micromagneticmodel/energy/magnetoelastic.py
@@ -7,8 +7,10 @@ from .energyterm import EnergyTerm
 @uu.inherit_docs
 @ts.typesystem(B1=ts.Parameter(descriptor=ts.Scalar(), otherwise=df.Field),
                B2=ts.Parameter(descriptor=ts.Scalar(), otherwise=df.Field),
-               e=ts.Parameter(descriptor=ts.Vector(size=6),
-                              otherwise=df.Field))
+               e_diag=ts.Parameter(descriptor=ts.Vector(size=3),
+                                   otherwise=df.Field),
+               e_offdiag=ts.Parameter(descriptor=ts.Vector(size=3),
+                                      otherwise=df.Field))
 class MagnetoElastic(EnergyTerm):
     """Magneto-elastic energy term.
 
@@ -27,18 +29,19 @@ class MagnetoElastic(EnergyTerm):
         parameter is defined "per region") or ``discretisedfield.Field`` is
         passed.
 
-    e : (6,) array_like, dict, discretisedfield.Field
+    e_diag/e_offdiag : (3,) array_like, dict, discretisedfield.Field
 
         Symmetric strain matrix is assembled from the values of the vector e,
-        so that eps11 = e[0], eps22=e[1], eps33=e[2], eps23=eps32=e[3],
-        eps13=eps31=e[4], eps12=eps21=eps[5].
+        so that eps11 = e_diag[0], eps22=e_diag[1], eps33=e_diag[2],
+        eps23=eps32=e_offdiag[0], eps13=eps31=e_offdiag[1],
+        eps12=eps21=e_offdiag[2].
 
-        If a single length-6 array_like (tuple, list, ``numpy.ndarray``) is
+        If a single length-3 array_like (tuple, list, ``numpy.ndarray``) is
         passed, which consists of ``numbers.Real``, a spatially constant
         parameter is defined. For a spatially varying parameter, either a
-        dictionary, e.g. ``e={'region1': (1, 1, 1, 2, 3, 1), 'region2': (1, 1,
-        2, 3, 2, 1)}`` (if the parameter is defined "per region") or
-        ``discretisedfield.Field`` is passed.
+        dictionary, e.g. ``e={'region1': (1, 1, 1), 'region2': (1, 1, 1)}`` (if
+        the parameter is defined "per region") or ``discretisedfield.Field`` is
+        passed.
 
     Examples
     --------
@@ -46,13 +49,16 @@ class MagnetoElastic(EnergyTerm):
 
     >>> import micromagneticmodel as mm
     ...
-    >>> mel = mm.MagnetoElastic(B1=1e7, B2=1e7, e=(1, 1, 1, 1, 1, 1))
+    >>> mel = mm.MagnetoElastic(B1=1e7, B2=1e7, e_diag=(1, 1, 1),
+    ...                         e_offdiag=(0, 0, 0))
 
     2. Defining the magneto-elastic energy term using dictionary.
 
     >>> B1 = B2 = {'region1': 1e7, 'region2': 2e7}
-    >>> e = {'region1': (1, 1, 1, 0, 0, 0), 'region2': (1, 1, 1, 1, 1, 1)}
-    >>> mel = mm.MagnetoElastic(B1=B1, B2=B2, e=e)
+    >>> e_diag = {'region1': (1, 1, 1), 'region2': (2, 2, 2)}
+    >>> e_offdiag = {'region1': (0, 0, 0), 'region2': (0, 0, 0)}
+    >>> mel = mm.MagnetoElastic(B1=B1, B2=B2, e_diag=e_diag,
+    ...                         e_offdiag=e_offdiag)
 
     3. Defining the magneto-elastic energy term using
     ``discretisedfield.Field``.
@@ -62,20 +68,21 @@ class MagnetoElastic(EnergyTerm):
     >>> region = df.Region(p1=(0, 0, 0), p2=(5e-9, 5e-9, 5e-9))
     >>> mesh = df.Mesh(region=region, n=(5, 5, 5))
     >>> B1 = B2 = df.Field(mesh, dim=1, value=1e6)
-    >>> e = df.Field(mesh, dim=6, value=(1, 1, 1, 1, 1, 1))
-    >>> mel = mm.MagnetoElastic(B1=B1, B2=B2, e=e)
+    >>> e_diag = df.Field(mesh, dim=3, value=(1, 1, 1))
+    >>> mel = mm.MagnetoElastic(B1=B1, B2=B2, e_diag=e_diag,
+    ...                         e_offdiag=(0, 0, 0))
 
     4. An attempt to define the magneto-elastic energy term using a wrong
     value.
 
     >>> # length-3 e value
-    >>> mel = mm.MagnetoElastic(B1=1e7, B2=2e7, e=(1, 1, 1))
+    >>> mel = mm.MagnetoElastic(B1=1e7, B2=2e7, e_diag=(1, 1, 1, 1))
     Traceback (most recent call last):
     ...
     ValueError: ...
 
     """
-    _allowed_attributes = ['B1', 'B2', 'e']
+    _allowed_attributes = ['B1', 'B2', 'e_diag', 'e_offdiag']
     _reprlatex = (r'B_{1}\sum_{i} m_{i}\epsilon_{ii} + '
                   r'B_{2}\sum_{i}\sum_{j\ne i} m_{i}m_{j}\epsilon_{ij}')
 
--- micromagneticmodel/tests/test_magnetoelastic.py
+++ micromagneticmodel/tests/test_magnetoelastic.py
@@ -11,32 +11,37 @@ class TestMagnetoElastic:
         mesh = df.Mesh(p1=(0, 0, 0), p2=(5, 5, 5), cell=(1, 1, 1))
         B1field = df.Field(mesh, dim=1, value=5e6)
         B2field = df.Field(mesh, dim=1, value=7e6)
-        efield = df.Field(mesh, dim=6, value=(1, 0, 0, 1, 1, 1))
+        e_diagfield = df.Field(mesh, dim=3, value=(1, 0, 0))
+        e_offdiagfield = df.Field(mesh, dim=3, value=(1, 1, 0))
 
-        self.valid_args = [(1, 1, (0, 1, 0, 2, 3, 7)),
-                           (2e6, 3e6, (1e6, 1, 0.1, 2, 3, 7)),
-                           (7e6, -2e-6, (0, 1e-8, 0, 2, 3.14, 7)),
-                           (B1field, B2field, efield)]
-        self.invalid_args = [((0, 1), 1, (0, 1, 0, 2, 3, 7)),
-                             (2e6, '5', (1e6, 1, 0.1, 2, 3, 7)),
-                             (7e6, -2e-6, (0, 1e-8, 3.14, 7)),
-                             (7e6, -2e-6, 5)]
+        self.valid_args = [(1, 1, (0, 1, 0), (2, 3, 7)),
+                           (2e6, 3e6, (1e6, 1, 0.1), (2, 3, 7)),
+                           (7e6, -2e-6, (0, 1e-8, 0), (2, 3.14, 7)),
+                           (B1field, B2field, e_diagfield, e_offdiagfield)]
+        self.invalid_args = [((0, 1), 1, (0, 1, 0), (2, 3, 7)),
+                             (2e6, '5', (1e6, 1, 0.1), (2, 3, 7)),
+                             (7e6, -2e-6, (0, 1e-8, 3.14), (1, 7)),
+                             (7e6, -2e-6, 5, 2)]
 
     def test_init_valid_args(self):
-        for B1, B2, e in self.valid_args:
-            term = mm.MagnetoElastic(B1=B1, B2=B2, e=e)
+        for B1, B2, e_diag, e_offdiag in self.valid_args:
+            term = mm.MagnetoElastic(B1=B1, B2=B2, e_diag=e_diag,
+                                     e_offdiag=e_offdiag)
             check_term(term)
             assert hasattr(term, 'B1')
             assert hasattr(term, 'B2')
-            assert hasattr(term, 'e')
+            assert hasattr(term, 'e_diag')
+            assert hasattr(term, 'e_offdiag')
             assert term.name == 'magnetoelastic'
-            assert re.search(r'^MagnetoElastic\(B1=.+, B2=.+, e=.+\)$',
+            assert re.search((r'^MagnetoElastic\(B1=.+, B2=.+, '
+                              r'e_diag=.+\, e_offdiag=.+\)$'),
                              repr(term))
 
     def test_init_invalid_args(self):
-        for B1, B2, e in self.invalid_args:
+        for B1, B2, e_diag, e_offdiag in self.invalid_args:
             with pytest.raises((TypeError, ValueError)):
-                term = mm.MagnetoElastic(B1=B1, B2=B2, e=e)
+                term = mm.MagnetoElastic(B1=B1, B2=B2, e_diag=e_diag,
+                                         e_offdiag=e_offdiag)
 
         with pytest.raises(AttributeError):
             term = mm.MagnetoElastic(wrong=1)

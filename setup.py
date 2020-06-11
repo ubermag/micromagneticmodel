0f0bb5812bda4b88f3e272e2bc124a5871c16d9b
Marijan Beg <m.beg@soton.ac.uk>
2020-06-11 23:16:41 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='micromagneticmodel',
-    version='0.8.17',
+    version='0.8.18',
     description=('Python domain-specific language for '
                  'defining micromagnetic models'),
     long_description=long_description,
@@ -14,14 +14,19 @@ setuptools.setup(
     author='Marijan Beg and Hans Fangohr',
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=['discretisedfield'],
-    classifiers=['Development Status :: 3 - Alpha',
+    python_requires='>=3.6',
+    install_requires=['discretisedfield==0.8.12'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Intended Audience :: Education',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
                  'License :: OSI Approved :: BSD License',
-                 'Programming Language :: Python :: 3 :: Only',
-                 'Operating System :: Unix',
+                 'Natural Language :: English',
                  'Operating System :: MacOS',
                  'Operating System :: Microsoft :: Windows',
+                 'Operating System :: Unix',
+                 'Programming Language :: Python :: 3 :: Only',
                  'Topic :: Scientific/Engineering :: Physics',
-                 'Intended Audience :: Science/Research',
-                 'Natural Language :: English']
+                 'Topic :: Scientific/Engineering :: Mathematics',
+                 'Topic :: Scientific/Engineering :: Visualization']
 )

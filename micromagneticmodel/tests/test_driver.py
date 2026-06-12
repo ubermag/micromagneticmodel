9ef21ffa26d0db69b7a766f9908a9dcf971ed5f9
Martin Lang <martin.lang@mpsd.mpg.de>
2026-06-12 16:26:29 +0200

    Extend tests and improve test documentation

--- micromagneticmodel/tests/test_driver.py
+++ micromagneticmodel/tests/test_driver.py
@@ -19,6 +19,11 @@ class MyDriver(adapter_base.Driver):
 
 
 def test_driver():
+    # The `Driver` base class imposes no restrictions on anything and only defines
+    # the `drive` method as public API. It needs to take a `system` (but does not
+    # enforce any checks on it) and should return the updated system.
+    # For simplicity, the `MyDriver` implementation just returns the system passed in
+    # so we can use an integer
     driver = MyDriver()
     assert driver.drive(system=5) == 5
     assert driver._x == "independent_variable"
@@ -61,13 +66,18 @@ class MyExternalDriver(adapter_base.ExternalDriver):
 
 
 def test_external_driver(tmp_path):
+    # This test contains multiple invocations of drive/schedule so that we can test
+    # the creation of multiple drive directories in the same system directory
     system = mm.examples.macrospin()
     driver = MyExternalDriver(arg1="a", arg2="b")
     assert driver._x == "x"
 
-    driver.drive(system, dirname=str(tmp_path))
+    # == first invocation ==
+    # check input and output files
+    driver.drive(system, dirname=tmp_path)
     m_out = df.Field.from_file(tmp_path / system.name / "drive-0" / "output.omf")
     assert system.m.allclose(m_out)
+    # MyExternalDriver inverts magnetisation in every call to drive
     assert system.m.allclose(-mm.examples.macrospin().m)
     assert (tmp_path / system.name / "drive-0" / "info.json").exists()
 
@@ -91,28 +101,39 @@ def test_external_driver(tmp_path):
     elapsed_time = info["elapsed_time"]
     elapsed_seconds = _parse_time_str_to_seconds(elapsed_time)
     # assumption: this test runs in under one minute
-    assert elapsed_seconds < 60
+    # we round to full seconds, so it should be > 0
+    assert 0 < elapsed_seconds < 60
 
+    # == second invocation ==
+    # calling drive with append=False fails if the system directory exists already
     with pytest.raises(FileExistsError):
         driver.drive(system, dirname=str(tmp_path), append=False)
 
-    # There is no scheduling system available for the tests. Instead we use 'python'
-    # because we know that this is always an executable. The created schedule script
-    # contains only Python comments so nothing is actually happening.
+    # == third invocation ==
+    # There is no scheduling system available for the tests, instead we use 'python'.
+    # The created schedule script contains only Python comments so nothing is actually
+    # happening.
     driver.schedule(system, "python", "#Schedule header", dirname=str(tmp_path))
     assert (tmp_path / system.name / "drive-1" / "macrospin.input").exists()
     assert (tmp_path / system.name / "drive-1" / "info.json").exists()
     assert (tmp_path / system.name / "drive-1" / "job.sh").exists()
+    # the simulation is never executed, so no output can exist
+    assert not (tmp_path / system.name / "drive-1" / "output.omf").exists()
+    # scheduling does not update the system, so we still have the state of the first run
+    assert system.m.allclose(-mm.examples.macrospin().m)
 
-    # Schedule header from file and runtime error during schedule.
+    # == fourth invocation ==
+    # Schedule header from file
     with (tmp_path / "header.sh").open("wt", encoding="utf-8") as f:
         f.write("import sys\nsys.exit(1)")
     with pytest.raises(RuntimeError):
+        # the schedule header calls `exit(1)`, meaning the scheduling fails
         driver.schedule(
             system, "python", str(tmp_path / "header.sh"), dirname=str(tmp_path)
         )
-        assert (tmp_path / system.name / "drive-2" / "macrospin.input").exists()
-        assert (tmp_path / system.name / "drive-2" / "info.json").exists()
-        assert (tmp_path / system.name / "drive-2" / "job.sh").exists()
+    # despite the failed scheduling, the input files have been written
+    assert (tmp_path / system.name / "drive-2" / "macrospin.input").exists()
+    assert (tmp_path / system.name / "drive-2" / "info.json").exists()
+    assert (tmp_path / system.name / "drive-2" / "job.sh").exists()
 
     assert len(list((tmp_path / system.name).glob("drive*"))) == 3
